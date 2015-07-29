73a7f3f6e3f0df80e6e53ac4448ff4ede3f1c9d7
duynt61089 <ntd12114@gmail.com>
2015-07-29 22:47:59 +0700

    add

---
+++ symfony/lib/controller/default/templates/defaultLayout.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <?php include_stylesheets() ?>
+</head>
+<body>
+<div class="container-fluid container-main">
+    <section id="content">
+        <!-- Begin Content -->
+
+        <div class="row-fluid">
+            <div class="span12">
+
+                <div id="system-message-container">
+                </div>
+                <div class="row-fluid">
+                    <div class="span2">
+                        <div class="sidebar-nav">
+                            <ul class="nav nav-list">
+                                <li class="nav-header">Trang Quản Trị</li>
+                                <li class="active"><a >Hỗ trợ kỹ thuật</a></li>
+                                <li><a >Nguyễn Trọng Duy  01682289699</a></li>
+                                <li><a >Nguyễn Viết Công</a></li>
+                                <li><a >Hứa Trung Anh</a></li>
+                                <li><a >Nguyễn Ngọc Phi</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="span6">
+                        <div class="well well-small"><div class="module-title nav-header">Quản lý người dùng hệ thống</div><div class="row-striped">
+                                <div class="row-fluid">
+                                    <div class="span9">
+
+                                        <strong class="row-title">
+                                            <a class="hasTooltip" href="">
+                                                Quản lý người dùng Backend						</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span9">
+
+                                        <strong class="row-title">
+                                            <a class="hasTooltip" href="">
+                                                Quản lý người dùng Frontend					</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="well well-small"><div class="module-title nav-header">Quản lý doanh nghiệp</div><div class="row-striped">
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">1</span>
+
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý doanh nghiệp</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">2</span>
+
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý loại hình doanh nghiệp </a>
+                                        </strong>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">3</span>
+
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý bảng xếp hạng doanh nghiệp</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--    du an-->
+                        <div class="well well-small"><div class="module-title nav-header">Dự án bất động sản</div><div class="row-striped">
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">1</span>
+
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý dự án</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">2</span>
+
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý loại hình dự án </a>
+                                        </strong>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">3</span>
+
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý dự án nổi bật</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--    end du an-->
+                        <div class="well well-small"><div class="module-title nav-header">Nhà môi giới</div><div class="row-striped">
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">1</span>
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý nhà môi giới</a>
+                                        </strong>
+                                    </div>
+
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">2</span>
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý nhà môi giới nổi bật</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--    orther-->
+                        <div class="well well-small"><div class="module-title nav-header">Khác</div><div class="row-striped">
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">1</span>
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý sự kiện</a>
+                                        </strong>
+                                    </div>
+
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span9">
+                                        <span class="badge badge-info hasTooltip" data-original-title="Hits">2</span>
+                                        <strong class="row-title">
+                                            <a href="">
+                                                Quản lý tỉnh thành</a>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--    end orther-->
+                    </div>
+                    <div class="span4">
+                        <div class="well well-small"><div class="module-title nav-header">Tin tức</div>	<div class="row-striped">
+                                <div class="row-fluid">
+                                    <div class="span12">
+                                        <a href="">
+                                            <span class="badge badge-info hasTooltip" data-original-title="Hits">1</span> <span>Quản lý tin tức chung</span>		</a>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span12">
+                                        <a href="">
+                                            <span class="badge badge-info hasTooltip" data-original-title="Hits">2</span> <span>Quản lý tin dự án</span>		</a>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span12">
+                                        <a href="">
+                                            <span class="badge badge-info hasTooltip" data-original-title="Hits">3</span><span>Quản lý tin doanh nghiệp</span>		</a>
+                                    </div>
+                                </div>
+                                <div class="row-fluid">
+                                    <div class="span12">
+                                        <a href="">
+                                            <span class="badge badge-info hasTooltip" data-original-title="Hits">1</span> <span>Quản lý tin do người dùng đăng</span>		</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>	</div>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- End Content -->
+    </section>
+    <hr>
+</div>
+</body>
+</html>
